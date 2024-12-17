6b8017030a81ae9911e53b0572f30fdfe764dab0
kasidin <kasidinkhamfoo@gmail.com>
2024-12-17 16:09:23 +0700

    lab10_2.cpp completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -10,6 +10,12 @@ int main (){
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	dest << "-------------------- BOOM ---------------------\n";
+	string tl;
+	while(getline(source,tl)){
+		dest << tl <<"\n";
+	}
+	dest << "-------------------- HA!! ---------------------";
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
